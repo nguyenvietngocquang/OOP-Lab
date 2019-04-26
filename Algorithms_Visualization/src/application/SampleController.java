793d79b9f7889fd7c64a2d68b7e8beac5479ab9d
Phung The Hung <hungpt@talent.ominext.com>
2019-04-26 14:32:12 +0700

    Created

---
+++ Algorithms_Visualization/src/application/SampleController.java
@@ -0,0 +1,19 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
+public class SampleController implements Initializable{
+	@FXML
+	private ChoiceBox<Label> choiceBox;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+	}
+}
