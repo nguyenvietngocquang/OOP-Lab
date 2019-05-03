9f415b762e44128058c073752c9583a612821b54
Phung The Hung <hungpt@talent.ominext.com>
2019-05-03 14:29:33 +0700

    First screen

--- Algorithms_Visualization/src/application/SampleController.java
+++ Algorithms_Visualization/src/application/SampleController.java
@@ -3,17 +3,48 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class SampleController implements Initializable{
 	@FXML
-	private ChoiceBox<Label> choiceBox;
+	
+	private Button queueBtn, arrayListBtn, hashTableBtn;
+	private Label lb;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		String mess = "";
+		queueBtn.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Done!");
+				
+			}
+		});
+	}
+	
+	public void menuBtn (ActionEvent event) {
+		String mess = "";
+		queueBtn.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Done!");
+				
+			}
+		});
+		if (arrayListBtn.isPressed()) {
+			mess += arrayListBtn.getText() + "\n";
+		}
+		if (hashTableBtn.isPressed()) {
+			mess += hashTableBtn.getText() + "\n";
+		}
+		lb.setText(mess);
 	}
 }
