8200f526469ca13dcece3feebc22da951817347f
Phung The Hung <hungpt@talent.ominext.com>
2019-05-09 21:33:10 +0700

    Update button

--- Algorithms_Visualization/src/application/ArrayListController.java
+++ Algorithms_Visualization/src/application/ArrayListController.java
@@ -24,6 +24,8 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class ArrayListController extends Application{
 
@@ -44,6 +46,7 @@ public class ArrayListController extends Application{
 	private TextField valueAddField;
 	@FXML 
 	public Button btnAddItem;
+	
 	@FXML
 	public void addItem(ActionEvent event) {
 		btnAddItem.setStyle("-fx-background-color: #DD5656");
@@ -62,7 +65,26 @@ public class ArrayListController extends Application{
 			statusText.setText("Done!");
 		}
 	}
-	
+	@FXML
+	public void addEnter(KeyEvent event) {
+	    if (event.getCode() == KeyCode.ENTER) {
+	    	btnAddItem.setStyle("-fx-background-color: #DD5656");
+			btnInsertItem.setStyle("-fx-background-color: #60d92e");
+			btnDeleteItem.setStyle("-fx-background-color: #60d92e");
+			value = valueAddField.getText();
+			if (valueAddField.getText().isEmpty()) statusText.setText("Invalid");
+			else {
+				StackPane obj = createShape(value);
+				listPane.add(obj);
+				setListPane(listPane);
+				arrayListPane.getChildren().add(obj);
+				int y = listPane.size()-1;
+				getWay(obj, y);
+				valueAddField.clear();
+				statusText.setText("Done!");
+			}
+	    }
+	}
 	@FXML
 	private TextField valueInsertField;
 	@FXML 
@@ -74,33 +96,71 @@ public class ArrayListController extends Application{
 		btnAddItem.setStyle("-fx-background-color: #60d92e");
 		btnInsertItem.setStyle("-fx-background-color: #DD5656");
 		btnDeleteItem.setStyle("-fx-background-color: #60d92e");
-		if (valueInsertField.getText().isEmpty() || indexInsertField.getText().isEmpty()) statusText.setText("Invalid");
+		if (indexInsertField.getText().isEmpty()) statusText.setText("Invalid Index");
 		else {
-			value = valueInsertField.getText();
-			int index = Integer.parseInt(indexInsertField.getText());
-			if (index > listPane.size()) statusText.setText("Wrong index!");
+			if (valueInsertField.getText().isEmpty()) statusText.setText("Invalid Value");
 			else {
-				StackPane obj = createShape(value);
-				if (index == 0) {
-					for (int i=0; i<listPane.size(); i++) {
-						goDown(listPane.get(i), i);	
+				value = valueInsertField.getText();
+				int index = Integer.parseInt(indexInsertField.getText());
+				if (index > listPane.size()) statusText.setText("Wrong index!");
+				else {
+					StackPane obj = createShape(value);
+					if (index == 0) {
+						for (int i=0; i<listPane.size(); i++) {
+							goDown(listPane.get(i), i);	
+						}
 					}
-				}
-				else if (index > 0) {
-					for (int i=index; i<listPane.size(); i++) {
-						goDown(listPane.get(i), i-index+1);
+					else if (index > 0) {
+						for (int i=index; i<listPane.size(); i++) {
+							goDown(listPane.get(i), i-index+1);
+						}
 					}
+					arrayListPane.getChildren().add(obj);
+					getWay(obj, index);
+					listPane.add(index, obj);
+					statusText.setText(""+listPane.size());
 				}
-				arrayListPane.getChildren().add(obj);
-				getWay(obj, index);
-				listPane.add(index, obj);
-				statusText.setText(""+listPane.size());
+				valueInsertField.clear();
+				indexInsertField.clear();
 			}
-			valueInsertField.clear();
-			indexInsertField.clear();
 		}
 	}
-	
+	@FXML
+	public void insertEnter(KeyEvent event) {
+	    if (event.getCode() == KeyCode.ENTER) {
+	    	btnAddItem.setStyle("-fx-background-color: #60d92e");
+			btnInsertItem.setStyle("-fx-background-color: #DD5656");
+			btnDeleteItem.setStyle("-fx-background-color: #60d92e");
+			if (indexInsertField.getText().isEmpty()) statusText.setText("Invalid Index");
+			else {
+				if (valueInsertField.getText().isEmpty()) statusText.setText("Invalid Value");
+				else {
+					value = valueInsertField.getText();
+					int index = Integer.parseInt(indexInsertField.getText());
+					if (index > listPane.size()) statusText.setText("Wrong index!");
+					else {
+						StackPane obj = createShape(value);
+						if (index == 0) {
+							for (int i=0; i<listPane.size(); i++) {
+								goDown(listPane.get(i), i);	
+							}
+						}
+						else if (index > 0) {
+							for (int i=index; i<listPane.size(); i++) {
+								goDown(listPane.get(i), i-index+1);
+							}
+						}
+						arrayListPane.getChildren().add(obj);
+						getWay(obj, index);
+						listPane.add(index, obj);
+						statusText.setText(""+listPane.size());
+					}
+					valueInsertField.clear();
+					indexInsertField.clear();
+				}
+			}
+	    }
+	}
 	@FXML
 	private TextField indexDeleteField;
 	@FXML 
@@ -131,6 +191,35 @@ public class ArrayListController extends Application{
 			}
 			indexDeleteField.clear();
 		}
+	}
+	@FXML
+	public void deleteEnter(KeyEvent event) {
+	    if (event.getCode() == KeyCode.ENTER) {
+	    	btnAddItem.setStyle("-fx-background-color: #60d92e");
+			btnInsertItem.setStyle("-fx-background-color: #60d92e");
+			btnDeleteItem.setStyle("-fx-background-color: #DD5656");
+			if (indexDeleteField.getText().isEmpty()) statusText.setText("Invalid!");
+			else {
+				int index = Integer.parseInt(indexDeleteField.getText());
+				if (index > listPane.size()) statusText.setText("Wrong index!");
+				else {
+					statusText.setText("Done!");
+					if (index == 0) {
+						for (int i=index+1; i<listPane.size(); i++) {
+							goUp(listPane.get(i), i-1);	
+						}
+					}
+					else if (index > 0){
+						for (int i=index; i<listPane.size(); i++) {
+							goUp(listPane.get(i), i-index);
+						}
+					}
+					arrayListPane.getChildren().remove(listPane.get(index));
+					listPane.remove(index);
+				}
+				indexDeleteField.clear();
+			}
+	    }
 	}
 	public ArrayList<StackPane> getListPane() {
 		return listPane;
@@ -194,6 +283,7 @@ public class ArrayListController extends Application{
 			AnchorPane root = FXMLLoader.load(getClass().getResource("ArrayList.fxml"));
 			Scene scene = new Scene(root,1000,600);
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("ArrayList");
 			primaryStage.show();
 		} catch(Exception e) {
 			System.out.println("Error!");
